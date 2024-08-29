f3ee679bdc9c423ca664f37ee372997da484e705
gbatistela <giulianobatistela@gmail.com>
2024-08-29 12:59:10 -0300

    aa

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const Contact: React.FC = () => {
   return (
     <section id="contact">
-      <h2>Contact Me</h2>
+      <h2>Contacto</h2>
       <p>
         You can reach me at:{" "}
-        <a href="mailto:giuliano@example.com">giuliano@example.com</a>
+        <a href="mailto:giuliano@example.com">giulianobatistela@gmail.com</a>
       </p>
     </section>
   );
