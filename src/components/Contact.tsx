aab3a4946833be8c1be4355b64297362c6df9e36
gbatistela <giulianobatistela@gmail.com>
2024-09-12 13:09:26 -0300

    ff

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,21 +8,25 @@ const Contact: React.FC = () => {
       <br></br>
       <div className="email-container">
         {/* Ícono de correo */}
-        <a href="https://mail.google.com/mail/u/0/#inbox">
-          <img src="/image.jpg" alt="Email Icon" className="email-icon" />
+        <a href="mailto:giulianobatistela@gmail.com">
+          <img
+            src="/icons/image.jpg" // Ruta desde la carpeta 'public'
+            alt="Email Icon"
+            className="email-icon"
+          />
         </a>
-        giulianobatistela@gmail.com
+        giulianobatist
       </div>
       <br></br>
       <br></br>
 
       <div className="whatsapp-container">
-        {/* Ícono de wp */}
+        {/* Ícono de WhatsApp */}
         <a href="https://wa.me/message/VOD76U3TLIVNK1">
           <img
-            src="public/icons/logowhatsapp.png"
-            alt="Email Icon"
-            className="email-icon"
+            src="/icons/logowhatsapp.png" // Ruta desde la carpeta 'public'
+            alt="WhatsApp Icon"
+            className="whatsapp-icon"
           />
         </a>
         +54 342 5543999
